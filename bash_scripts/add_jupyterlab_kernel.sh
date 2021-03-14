6243574e70de1f63208ec39ea932e56e8d7524e6
ubd <ubd@YogaFlame.YogaTeleport>
2021-03-14 16:12:42 +0100

    Initiate bash_scripts

---
+++ bash_scripts/add_jupyterlab_kernel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ~/anaconda/etc/profile.d/conda.sh
+conda activate $1
+conda install -y ipykernel
+ipython kernel install --user --name=$1
+conda deactivate
+
+echo Kernel $1 has been added to the jupyter-lab
